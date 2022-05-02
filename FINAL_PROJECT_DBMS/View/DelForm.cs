29ec2e9a37ad371c2552565f0a631d93ce3764f3
Tien Dat <tdat.it2k2@gmail.com>
2022-05-02 22:46:36 +0700

    aâ

--- FINAL_PROJECT_DBMS/View/DelForm.cs
+++ FINAL_PROJECT_DBMS/View/DelForm.cs
@@ -19,8 +19,7 @@ namespace FINAL_PROJECT_DBMS.View
 
         private void button2_Click(object sender, EventArgs e)
         {
-            DeleteEmployee deleteEmployee = new DeleteEmployee();
-            deleteEmployee.Visible = true;
+           
         }
 
         private void button3_Click(object sender, EventArgs e)
