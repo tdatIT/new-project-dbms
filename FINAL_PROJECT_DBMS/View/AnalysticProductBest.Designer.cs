d32f5781e67e262a1b69a4074c74152240b6c8c1
Admin <Admin@LAPTOP-VDUMRGAT>
2022-05-08 21:09:52 +0700

    Them 2 Fucntion va 3 View

---
+++ FINAL_PROJECT_DBMS/View/AnalysticProductBest.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace FINAL_PROJECT_DBMS.Model
+{
+    partial class AnalysticProductBest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgv_result = new System.Windows.Forms.DataGridView();
+            this.name_product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.so_luong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgv_result);
+            this.panel1.Location = new System.Drawing.Point(174, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 237);
+            this.panel1.TabIndex = 15;
+            // 
+            // dgv_result
+            // 
+            this.dgv_result.AllowUserToAddRows = false;
+            this.dgv_result.AllowUserToDeleteRows = false;
+            this.dgv_result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_result.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name_product,
+            this.so_luong});
+            this.dgv_result.Location = new System.Drawing.Point(57, 3);
+            this.dgv_result.Name = "dgv_result";
+            this.dgv_result.ReadOnly = true;
+            this.dgv_result.RowHeadersWidth = 51;
+            this.dgv_result.Size = new System.Drawing.Size(401, 211);
+            this.dgv_result.TabIndex = 0;
+            // 
+            // name_product
+            // 
+            this.name_product.DataPropertyName = "name_product";
+            this.name_product.Frozen = true;
+            this.name_product.HeaderText = "Tên sản phẩm";
+            this.name_product.MinimumWidth = 6;
+            this.name_product.Name = "name_product";
+            this.name_product.ReadOnly = true;
+            this.name_product.Width = 150;
+            // 
+            // so_luong
+            // 
+            this.so_luong.DataPropertyName = "so_luong";
+            this.so_luong.Frozen = true;
+            this.so_luong.HeaderText = "Số lượng";
+            this.so_luong.MinimumWidth = 6;
+            this.so_luong.Name = "so_luong";
+            this.so_luong.ReadOnly = true;
+            this.so_luong.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(226, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 28);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "SẢN PHẨM BÁN CHẠY NHẤT CỦA QUÁN";
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.Location = new System.Drawing.Point(449, 132);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(109, 30);
+            this.btn_enter.TabIndex = 18;
+            this.btn_enter.Text = "ENTER";
+            this.btn_enter.UseVisualStyleBackColor = true;
+            this.btn_enter.Click += new System.EventHandler(this.btn_enter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(227, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nhập vào [SHOP_ID]";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // AnalysticProductBest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(136)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_enter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AnalysticProductBest";
+            this.Text = "AnalysticProductBest";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgv_result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name_product;
+        private System.Windows.Forms.DataGridViewTextBoxColumn so_luong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ FINAL_PROJECT_DBMS/View/AnalysticTotalPayment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FINAL_PROJECT_DBMS.Model;
+
+namespace FINAL_PROJECT_DBMS.Model
+{
+    public partial class AnalysticTotalPayment : Form
+    {
+        DAOConnection db;
+        public AnalysticTotalPayment()
+        {
+            InitializeComponent();
+            db = new DAOConnection();
+        }
+
+        private void btn_enter_Click(object sender, EventArgs e)
+        {
+            string shopID = textBox1.Text;
+            DataTable data = db.showTotalPayment(shopID);
+            if (data != null)
+            {
+                dgv_result.DataSource = data;
+            }
+            else
+                MessageBox.Show("Không có dữ liệu !!!");
+        }
+    }
+}
