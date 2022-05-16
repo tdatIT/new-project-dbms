3a66101480454ef5a0f3a4b5451caf652ae90aeb
Admin <Admin@LAPTOP-VDUMRGAT>
2022-05-16 17:49:57 +0700

    Final commit Complete Project -Tran Tien Dat-

--- FINAL_PROJECT_DBMS/View/AddNewOrder.Designer.cs
+++ FINAL_PROJECT_DBMS/View/AddNewOrder.Designer.cs
@@ -91,7 +91,7 @@
             this.cbx_shop.FormattingEnabled = true;
             this.cbx_shop.Location = new System.Drawing.Point(120, 101);
             this.cbx_shop.Name = "cbx_shop";
-            this.cbx_shop.Size = new System.Drawing.Size(85, 28);
+            this.cbx_shop.Size = new System.Drawing.Size(85, 23);
             this.cbx_shop.TabIndex = 9;
             this.cbx_shop.SelectedIndexChanged += new System.EventHandler(this.cbx_shop_SelectedIndexChanged);
             // 
@@ -100,7 +100,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(14, 103);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(47, 20);
+            this.label9.Size = new System.Drawing.Size(38, 15);
             this.label9.TabIndex = 8;
             this.label9.Text = "SHOP";
             // 
@@ -119,7 +119,7 @@
             this.txt_year.Location = new System.Drawing.Point(183, 64);
             this.txt_year.Name = "txt_year";
             this.txt_year.ReadOnly = true;
-            this.txt_year.Size = new System.Drawing.Size(59, 27);
+            this.txt_year.Size = new System.Drawing.Size(59, 23);
             this.txt_year.TabIndex = 5;
             // 
             // txt_month
@@ -127,15 +127,14 @@
             this.txt_month.Location = new System.Drawing.Point(138, 64);
             this.txt_month.Name = "txt_month";
             this.txt_month.ReadOnly = true;
-            this.txt_month.Size = new System.Drawing.Size(33, 27);
+            this.txt_month.Size = new System.Drawing.Size(33, 23);
             this.txt_month.TabIndex = 4;
             // 
             // txt_day
             // 
             this.txt_day.Location = new System.Drawing.Point(85, 64);
             this.txt_day.Name = "txt_day";
-            this.txt_day.ReadOnly = true;
-            this.txt_day.Size = new System.Drawing.Size(35, 27);
+            this.txt_day.Size = new System.Drawing.Size(35, 23);
             this.txt_day.TabIndex = 3;
             // 
             // lb
@@ -143,7 +142,7 @@
             this.lb.AutoSize = true;
             this.lb.Location = new System.Drawing.Point(14, 64);
             this.lb.Name = "lb";
-            this.lb.Size = new System.Drawing.Size(45, 20);
+            this.lb.Size = new System.Drawing.Size(34, 15);
             this.lb.TabIndex = 2;
             this.lb.Text = "DATE";
             // 
@@ -152,7 +151,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.Size = new System.Drawing.Size(37, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Số ĐT";
             // 
@@ -160,7 +159,7 @@
             // 
             this.txt_c_phone.Location = new System.Drawing.Point(85, 25);
             this.txt_c_phone.Name = "txt_c_phone";
-            this.txt_c_phone.Size = new System.Drawing.Size(157, 27);
+            this.txt_c_phone.Size = new System.Drawing.Size(157, 23);
             this.txt_c_phone.TabIndex = 0;
             // 
             // pn_payment
@@ -185,7 +184,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(236, 104);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.Size = new System.Drawing.Size(31, 15);
             this.label5.TabIndex = 7;
             this.label5.Text = "VND";
             // 
@@ -204,7 +203,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(17, 103);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.Size = new System.Drawing.Size(63, 15);
             this.label4.TabIndex = 5;
             this.label4.Text = "Thành tiền";
             // 
@@ -213,7 +212,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(17, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.Size = new System.Drawing.Size(50, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "Voucher";
             // 
@@ -222,7 +221,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(16, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.Size = new System.Drawing.Size(91, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Phương thức TT";
             // 
@@ -231,7 +230,7 @@
             this.txt_totalprice.Location = new System.Drawing.Point(121, 100);
             this.txt_totalprice.Name = "txt_totalprice";
             this.txt_totalprice.ReadOnly = true;
-            this.txt_totalprice.Size = new System.Drawing.Size(111, 27);
+            this.txt_totalprice.Size = new System.Drawing.Size(111, 23);
             this.txt_totalprice.TabIndex = 2;
             // 
             // cbx_voucher
@@ -240,7 +239,7 @@
             this.cbx_voucher.FormattingEnabled = true;
             this.cbx_voucher.Location = new System.Drawing.Point(121, 60);
             this.cbx_voucher.Name = "cbx_voucher";
-            this.cbx_voucher.Size = new System.Drawing.Size(182, 28);
+            this.cbx_voucher.Size = new System.Drawing.Size(182, 23);
             this.cbx_voucher.TabIndex = 1;
             // 
             // cbx_payment_method
@@ -253,7 +252,7 @@
             "Giao hàng"});
             this.cbx_payment_method.Location = new System.Drawing.Point(121, 22);
             this.cbx_payment_method.Name = "cbx_payment_method";
-            this.cbx_payment_method.Size = new System.Drawing.Size(182, 28);
+            this.cbx_payment_method.Size = new System.Drawing.Size(182, 23);
             this.cbx_payment_method.TabIndex = 0;
             // 
             // pn_product
@@ -283,7 +282,7 @@
             // 
             this.txt_amount.Location = new System.Drawing.Point(70, 210);
             this.txt_amount.Name = "txt_amount";
-            this.txt_amount.Size = new System.Drawing.Size(42, 27);
+            this.txt_amount.Size = new System.Drawing.Size(42, 23);
             this.txt_amount.TabIndex = 1;
             this.txt_amount.Text = "1";
             this.txt_amount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -357,7 +356,7 @@
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(75, 229);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(174, 25);
+            this.label6.Size = new System.Drawing.Size(136, 20);
             this.label6.TabIndex = 8;
             this.label6.Text = "DANH SÁCH MÓN";
             // 
@@ -367,7 +366,7 @@
             this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(75, 22);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(158, 25);
+            this.label7.Size = new System.Drawing.Size(124, 20);
             this.label7.TabIndex = 9;
             this.label7.Text = "TẠO ĐƠN HÀNG";
             // 
@@ -377,13 +376,13 @@
             this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(409, 25);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(134, 25);
+            this.label8.Size = new System.Drawing.Size(105, 20);
             this.label8.TabIndex = 11;
             this.label8.Text = "THANH TOÁN";
             // 
             // AddNewOrder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(727, 516);
             this.Controls.Add(this.label8);
